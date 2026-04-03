825d6b496ecaa59ef7d6f95214d39a68ad579cc9
qkryjsdn <qkryjsdn1928@gmail.com>
2026-04-03 14:53:00 +0900

    Create 4-7-6 sensor.py

---
+++ code/4장/과제/4-7-6 sensor.py
@@ -0,0 +1,13 @@
+sensors = {
+    'dht11': {
+        'temperature': 23,
+        'humidity': 47,
+        'unit': 'celsius'
+    },
+    'bh1750': {
+        'illuminance': 450,
+        'unit': 'lux'
+    }
+}
+
+sensors
