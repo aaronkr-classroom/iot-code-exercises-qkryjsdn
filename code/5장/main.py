4e16a83d758815edb56eff86efe14e66bbe27cf7
qkryjsdn <qkryjsdn1928@gmail.com>
2026-04-06 11:25:04 +0900

    실습

---
+++ code/5장/main.py
@@ -0,0 +1,20 @@
+# main.py
+# 모듈확인 help('module')
+
+from device import SensorDevice, ActuatorDevice, TemperatureSensor, LightSensor
+
+sensor = SensorDevice("Temp Sensor")
+actuator = ActuatorDevice("LED Contorller")
+
+sensor.connect()
+actuator.connect()
+
+print(sensor.status())
+print(actuator.status())
+print()
+
+temp = TemperatureSensor("Temp1")
+light = LightSensor("Light1")
+
+print("Temp: ", temp.read())
+print("Light: ", light.read())
